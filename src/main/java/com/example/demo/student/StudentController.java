8153f273186efa9a8d97cf16c0120d92de57ce65
Steph Peets <stephanie.peets@gmail.com>
2022-01-16 23:18:37 -0500

    Add @Service to StudentService class, @Autowired StudentController constructor

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+// @RestController is specific type of component
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
 	private final StudentService studentService;
 
+	@Autowired
 	public StudentController(StudentService studentService) {
 		this.studentService = studentService;
 	}
