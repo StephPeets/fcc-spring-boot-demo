506f32398e45e07d6382cb0994c4b06d8ceb00bc
Steph Peets <stephanie.peets@gmail.com>
2022-01-17 21:40:17 -0500

    connect to postgres

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,7 +1,6 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-// @RestController is specific type of component
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
